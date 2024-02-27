c64a968d192829c07e58e1854e9e6b753729b136
rohit0814 <singhrohit.rs747@gmail.com>
2024-02-27 20:06:59 +0530

    ui of blogPage completed

--- Context-Api-practices/src/Components/BlogPage.jsx
+++ Context-Api-practices/src/Components/BlogPage.jsx
@@ -2,17 +2,42 @@ import style from './blogpage.module.css';
 
 const Blogpage = () => {
     return <>
-        <div class="container my-3">
-    <div class="bg-body-tertiary p-3 rounded">
-      <div class="col-sm-8 py-3 mx-auto">
-        <input type="text" className={style.heading}  placeholder='Title'/><br />
-        <textarea className={style.title}></textarea>
-        <p>
-          <a class="btn btn-secondary" href="/docs/5.3/components/navbar/#offcanvas" role="button">Publish »</a>
-        </p>
+        <section class="">
+  <h1 class="shadow-sm text-center py-3">Write Your blog</h1>
+    <div class="flex-box">
+      <div class="row">
+        <center>
+          <input type='text' id="heading" className={`input shadow ${style.heading}`} name="name" placeholder="Your Title here " />
+          </center>
+      </div><br />
+      <div class="row py-2">
+        <div class="col d-flex justify-content-center gap-2">
+          <button type="button" onclick="f1()" class=" shadow-sm btn btn-outline-secondary" data-toggle="tooltip" data-placement="top" title="Bold Text">Bold</button>
+          <button type="button" onclick="f2()" class="shadow-sm btn btn-outline-success" data-toggle="tooltip" data-placement="top" title="Italic Text">Italic</button>
+          <button type="button" onclick="f3()" class=" shadow-sm btn btn-outline-primary" data-toggle="tooltip" data-placement="top" title="Left Align"><i class="fas fa-align-left"></i></button>
+          <button type="button" onclick="f4()" class="btn shadow-sm btn-outline-secondary" data-toggle="tooltip" data-placement="top" title="Center Align"><i class="fas fa-align-center"></i></button>
+          <button type="button" onclick="f5()" class="btn shadow-sm btn-outline-primary" data-toggle="tooltip" data-placement="top" title="Right Align"><i class="fas fa-align-right"></i></button>
+          <button type="button" onclick="f6()" class="btn shadow-sm btn-outline-secondary" data-toggle="tooltip" data-placement="top" title="Uppercase Text">Upper Case</button>
+          <button type="button" onclick="f7()" class="btn shadow-sm btn-outline-primary" data-toggle="tooltip" data-placement="top" title="Lowercase Text">Lower Case</button>
+          <button type="button" onclick="f8()" class="btn shadow-sm btn-outline-primary" data-toggle="tooltip" data-placement="top" title="Capitalize Text">Capitalize</button>
+          <button type="button" onclick="f9()" class="btn shadow-sm btn-outline-primary side" data-toggle="tooltip" data-placement="top" title="Tooltip on top">Clear Text</button>
+          <button type="button" onclick="f10()" class="btn shadow-sm btn-outline-primary side" data-toggle="tooltip" data-placement="top" title="Download">Publish <i class="fa-solid fa-upload"></i></button>
+        </div>
+      </div>
     </div>
+    <br />
+    <div class="row">
+      <div class="col-md-3 col-sm-3">
+      </div>
+      <div class="col-md-6 col-sm-9">
+        <div class="flex-box">
+          <textarea id="textarea1" class="input shadow p-3" name="name" rows="15" cols="100" placeholder="Your text here "></textarea>
+        </div>
+      </div>
+      <div class="col-md-3">
+      </div>
     </div>
-  </div>
+  </section>
     </>
 }
 
